a7914e04505037a2de4caf98f1210262318d8cc8
Kevinjoelclief29 <kevinjoelclief57@gmail.com>
2024-01-24 15:53:29 +0800

    exercise 2

---
+++ members.js
@@ -0,0 +1,15 @@
+const membersData = [
+    { id: 1, nim: '12345', nama: 'Clief, Kevin Joel', nomorTelp: '081242577286' },
+    { id: 2, nim: '12345', nama: 'Rionaldo Siaman', nomorTelp: '082292909191' },
+    { id: 3, nim: '12345', nama: 'Fransdito Emor', nomorTelp: '082243577286' },
+    { id: 4, nim: '12345', nama: 'Falen Tombeng', nomorTelp: '081237829023' },
+  ];
+  
+  function getMembers() {
+    return membersData;
+  }
+  
+  module.exports = {
+    getMembers
+  };
+  
